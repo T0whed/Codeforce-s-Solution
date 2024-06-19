5b7127cb1ccae9481822ab9187f3a63ddff4c8f0
Towheduzzaman <166198860+T0whed@users.noreply.github.com>
2024-06-19 15:45:50 +0600

    Add files via upload

---
+++ CreatingWords.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define int long long int
+#define endl "\n"
+const int N = 1e6 + 7;
+
+void stdio() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    string a, b; cin >> a >> b;
+    char c = a[0], ch = b[0];
+    b[0] = c; a[0] = ch;
+    cout << a << ' ' << b << '\n';
+}
+
+int32_t main() {
+    FAST
+    stdio();
+    int t = 1, i = 0; cin >> t;
+    while (t -- > 0) {
+        //cout << "Case " << ++i << ": ";
+        solve();
+    }
+    return 0;
+}
---
+++ MaximumMultipleSum.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define int long long int
+#define endl "\n"
+const int N = 1e6 + 7;
+
+void stdio() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    int n; cin >> n;
+    cout << (n == 3 ? 3 : 2) << "\n";
+}
+
+int32_t main() {
+    FAST
+    stdio();
+    int t = 1, i = 0; cin >> t;
+    while (t -- > 0) {
+        //cout << "Case " << ++i << ": ";
+        solve();
+    }
+    return 0;
+}
